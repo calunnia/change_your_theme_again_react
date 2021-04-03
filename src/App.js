0bb4ff3c95d4753d622933d73a56983a4fb49334
Erika Szilagyi <calunnia@gmail.com>
2021-04-03 15:18:15 +0200

    added mockserver

--- src/App.js
+++ src/App.js
@@ -1,6 +1,55 @@
 import './App.css'
+import React, {useEffect, useState} from 'react'
 
 const App = () => {
+
+  
+  const [data, setData] = useState([])
+  const [loading, setloading] = useState(false)
+  const [show, setShow] = useState('')
+  const [moreInfo, setMoreInfo] = useState(true)
+  
+
+  const [refresh, setRefresh] = useState(false)
+  const [filteredData, setFilteredData] = useState([])
+ 
+
+useEffect(() => {
+ 
+      setloading(true)
+      setData([])           //elozo adatok ne latszodjanak
+      setFilteredData([])
+      let url = "/api/data" // mockserverURL
+
+      url = "/api/filterData?search=Titanok" // mockserverURL
+
+
+
+      console.log('fetch from ',url);
+    
+      fetch(url)
+      .then(  (response) => (response.json()))
+      .then(  (respAdat)     => (setData(respAdat)))
+      .catch( (error)    => {
+                              console.log('error', error)
+                              setData(null)
+                            }
+            )
+      .finally( ()=>{
+      
+                      console.log('fetch end');
+                      setloading(false)
+                    }
+              )
+      return () => {
+      // cleanup
+      }
+}, [refresh])
+
+console.log('movies data=',data);
+
+
+
 
   return (
     <div className="App">
